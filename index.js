0c85df2206c673b0a7e874a91ce899c342d152cd
Yorman Amaro <amarocarrero24@gmail.com>
2022-09-06 22:35:52 -0300

    Project init

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const h1 = document.
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log(h1);
+
+console.log(input.value); // elemento que nmos permite ver que escribe el usuario
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input,
+});

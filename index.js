f4db9fffebb1fd4705ae30bfe555c80578e616c1
Yorman Amaro <amarocarrero24@gmail.com>
2022-09-08 18:56:50 -0300

    class-event-in-js-html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const h1 = document.querySelector('h1');
++ngreseconst h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.querySelector('.parrafito');
 const pid = document.querySelector('#pid');
@@ -6,7 +6,7 @@ const input = document.querySelector('input');
 
 console.log(h1);
 
-console.log(input.value); // elemento que nmos permite ver que escribe el usuario
+console.log(input.value); // elemento que nos permite ver que escribe el usuario
 
 console.log({
     h1,
@@ -15,3 +15,23 @@ console.log({
     pid,
     input,
 });
+
+
+h1.innerHTML = 'Esto es un ejemplo de modificar un h1 con JS <br> Otro ejemplo de modificacion de htm en js';
+
+
+//  PARA UBICAR CLASES ASIGNADAS A ETIQUETAS
+
+console.log(h1.getAttribute('verde'));
+h1.setAttribute('verde', 'rojo');
+
+
+h1.classList.add('naranja'); // Para agregar clases desde JS
+h1.classList.remove('rojo'); // Para remover clases desde JS
+
+input.value = "Ingrese su Email"; // Para agregar un mensaje al input
+
+
+
+
+
